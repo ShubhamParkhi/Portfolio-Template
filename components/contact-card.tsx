b3aa239c14973eba89573008e76a851f6dee890e
Shubham Parkhi <shubhamparkhi10@gmail.com>
2023-10-27 20:26:39 +0530

    Implemented Tilt Effect

--- components/contact-card.tsx
+++ components/contact-card.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from "next";
 import Link from "next/link";
+import Parallax from 'react-parallax-tilt'
 
 const ContactCard: NextPage = () => {
   return (
     <section className="self-stretch flex flex-col items-center justify-center py-[75px] px-[30px] text-center text-21xl text-shade1 ">
-      <div className="w-full rounded-3xl bg-darkslategray box-border overflow-hidden flex flex-row items-start justify-center max-w-[1100px] border-[1px] border-solid border-shade-3">
+      <Parallax className="w-full rounded-3xl bg-darkslategray box-border overflow-hidden flex flex-row items-start justify-center max-w-[1100px] border-[1px] border-solid border-shade-3" style={{ transformStyle: 'preserve-3d' }}  glareEnable={true} glareColor="#9b5cff" glareMaxOpacity={0.08} glarePosition="all" tiltMaxAngleX={3} tiltMaxAngleY={3} gyroscope={true} transitionSpeed={1000}>
         <div className=" flex flex-col items-start justify-start">
           <img
             className=" w-[258px] h-[245px] object-cover"
@@ -42,8 +43,8 @@ const ContactCard: NextPage = () => {
             alt=""
             src="/ellipse-right@2x.png"
           />
-        </div>
       </div>
+      </Parallax>
     </section>
   );
 };
--- components/design-principles.tsx
+++ components/design-principles.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Parallax from 'react-parallax-tilt'
 
 const DesignPrinciples: NextPage = () => {
   return (
@@ -7,44 +8,37 @@ const DesignPrinciples: NextPage = () => {
         <h1 className="m-0 text-21xl font-medium sm:leading-[60px]">Eye Catching, Modern & Minimalist Design.</h1>
         <div className="w-full flex flex-row items-center gap-[50px] max-w-[1100px] text-5xl text-lightsteelblue sm:flex-col sm:gap-[50px] sm:items-center">
           <div className="flex-1 flex flex-col items-start gap-[50px]">
-            <div className="rounded-mini bg-darkorchid-300 flex flex-col items-start py-[25px] px-[30px] gap-[10px] border-[2px] border-solid border-darkorchid-100">
-              <div className="font-medium">
-                Typography
-              </div>
+            <Parallax  style={{ transformStyle: 'preserve-3d' }}  glareEnable={true} glareColor="#5de2ff" glareMaxOpacity={0.08} glarePosition="all" tiltMaxAngleX={5} tiltMaxAngleY={5} gyroscope={true} transitionSpeed={1000} className="rounded-mini bg-darkorchid-300 flex flex-col items-start py-[25px] px-[30px] gap-[10px] border-[2px] border-solid border-darkorchid-100">
+              <div className="font-medium">Typography</div>
               <div className="text-xl">
                 Selecting the font type, font size and font weight.
               </div>
-            </div>
-            <div className="rounded-mini bg-darkorchid-300 flex flex-col items-start py-[25px] px-[30px] gap-[10px] border-[2px] border-solid border-darkorchid-100">
+            </Parallax>
+            <Parallax  style={{ transformStyle: 'preserve-3d' }}  glareEnable={true} glareColor="#5de2ff" glareMaxOpacity={0.08} glarePosition="all" tiltMaxAngleX={5} tiltMaxAngleY={5} gyroscope={true} transitionSpeed={1000} className="rounded-mini bg-darkorchid-300 flex flex-col items-start py-[25px] px-[30px] gap-[10px] border-[2px] border-solid border-darkorchid-100">
               <div className="font-medium">Spacing</div>
               <div className="text-xl">
                 Positioning and adding space between elements.
               </div>
-            </div>
-            <div className="rounded-mini bg-darkorchid-300 flex flex-col items-start py-[25px] px-[30px] gap-[10px] border-[2px] border-solid border-darkorchid-100">
+            </Parallax>
+            <Parallax  style={{ transformStyle: 'preserve-3d' }}  glareEnable={true} glareColor="#5de2ff" glareMaxOpacity={0.08} glarePosition="all" tiltMaxAngleX={5} tiltMaxAngleY={5} gyroscope={true} transitionSpeed={1000} className="rounded-mini bg-darkorchid-300 flex flex-col items-start py-[25px] px-[30px] gap-[10px] border-[2px] border-solid border-darkorchid-100">
               <div className="font-medium">Colours</div>
               <div className="text-xl">
                 Choosing a colour scheme with sufficient contrast.
               </div>
-            </div>
-            <div className="rounded-mini bg-darkorchid-300 flex flex-col items-start py-[25px] px-[30px] gap-[10px] border-[2px] border-solid border-darkorchid-100">
+            </Parallax>
+            <Parallax  style={{ transformStyle: 'preserve-3d' }}  glareEnable={true} glareColor="#5de2ff" glareMaxOpacity={0.08} glarePosition="all" tiltMaxAngleX={5} tiltMaxAngleY={5} gyroscope={true} transitionSpeed={1000} className="rounded-mini bg-darkorchid-300 flex flex-col items-start py-[25px] px-[30px] gap-[10px] border-[2px] border-solid border-darkorchid-100">
               <div className="font-medium">Effects</div>
               <div className="text-xl">
                 Add effects like shadows, rounded corner, blur etc.
               </div>
-            </div>
+            </Parallax>
           </div>
           <div className="flex-1 flex flex-col items-center gap-[50px]">
-            <div className="rounded-mini bg-darkorchid-300 flex flex-col items-start py-[25px] px-[30px] gap-[10px] border-[2px] border-solid border-darkorchid-100">
-              <div className="font-medium">
-                Keen Eye for Spotting Small Details
-              </div>
-              <div className="text-xl">
-                Awareness to ease of access, User Interface consistency, and
-                improved User Experience.
-              </div>
-            </div>
-            <div className="rounded-mini bg-darkorchid-300 flex flex-col items-start py-[25px] px-[30px] gap-[10px] border-[2px] border-solid border-darkorchid-100">
+            <Parallax  style={{ transformStyle: 'preserve-3d' }}  glareEnable={true} glareColor="#5de2ff" glareMaxOpacity={0.08} glarePosition="all" tiltMaxAngleX={5} tiltMaxAngleY={5} gyroscope={true} transitionSpeed={1000} className="rounded-mini bg-darkorchid-300 flex flex-col items-start py-[25px] px-[30px] gap-[10px] border-[2px] border-solid border-darkorchid-100">
+              <div className="font-medium">Keen Eye for Spotting Small Details</div>
+              <div className="text-xl">Awareness to ease of access, User Interface consistency, and improved User Experience.</div>
+            </Parallax>
+            <Parallax  style={{ transformStyle: 'preserve-3d' }}  glareEnable={true} glareColor="#5de2ff" glareMaxOpacity={0.08} glarePosition="all" tiltMaxAngleX={5} tiltMaxAngleY={5} gyroscope={true} transitionSpeed={1000} className="rounded-mini bg-darkorchid-300 flex flex-col items-start py-[25px] px-[30px] gap-[10px] border-[2px] border-solid border-darkorchid-100">
               <div className="font-medium">
                 Comprehensible and Optimized Code
               </div>
@@ -52,7 +46,7 @@ const DesignPrinciples: NextPage = () => {
                 Writing clean code is a top priority while keeping it as
                 optimized as possible.
               </div>
-            </div>
+            </Parallax>
           </div>
         </div>
       </div>
