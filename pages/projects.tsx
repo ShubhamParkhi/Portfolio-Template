0c8897b7746ac36d970044115087a51e7ff2260f
Shubham Parkhi <shubhamparkhi10@gmail.com>
2023-11-24 04:23:34 +0530

    Update projects.tsx

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -8,46 +8,57 @@ import { createClient } from "@supabase/supabase-js";
 import { useEffect, useState } from "react";
 
 const Projects: NextPage = () => {
-  const client = createClient(process.env.NEXT_PUBLIC_URL as string, process.env.NEXT_PUBLIC_KEY as string);
-  const [projects, setProjects] = useState<{id: number; projectName: string; taskDescription1: string; taskDescription2: string; taskDescription3: string; githubLink: string; deployedLink: string; previewImageLink: string;}[] | null>(null);
+  const client = createClient(
+    process.env.NEXT_PUBLIC_URL as string,
+    process.env.NEXT_PUBLIC_KEY as string
+  );
+  const [projects, setProjects] = useState<
+    | {
+        id: number;
+        projectName: string;
+        taskDescription1: string;
+        taskDescription2: string;
+        taskDescription3: string;
+        githubLink: string;
+        deployedLink: string;
+        previewImageLink: string;
+      }[]
+    | null
+  >(null);
   useEffect(() => {
     const fetchProjects = async () => {
-      const result = await client.from("projects").select('*');
+      const result = await client.from("projects").select("*");
       setProjects(result.data);
       result.data!.sort((a, b) => a.id - b.id);
-    }
+    };
     fetchProjects();
   }, []);
 
   return (
     <div className="[text-decoration:none] bg-gray-100 w-full flex flex-col items-start justify-start font-poppins">
-      <NavBar
-        projectsColor="#7127ba"
-        contactColor="#fff"
-      />
+      <NavBar projectsColor="#7127ba" contactColor="#fff" />
       <HeadingContainer
         sectionTitle="Projects"
         developmentWorkDescription="Showcase of my development related work."
       />
-      <div>
-
-      </div>
+      <div></div>
       {projects === null ? (
-      <Loading height='80vh' width="80vh" />
-    ) : (
-      projects.map((project) => (
-        <ProjectContainer
-          key={project.id}
-          projectName={project.projectName}
-          taskDescription1={project.taskDescription1}
-          taskDescription2={project.taskDescription2}
-          taskDescription3={project.taskDescription3}
-          githubLink={project.githubLink}
-          deployedLink={project.deployedLink}
-          previewImage={project.previewImageLink}
-        />
-      ))
-    )}      <Footer />
+        <Loading height="80vh" width="80vh" />
+      ) : (
+        projects.map((project) => (
+          <ProjectContainer
+            key={project.id}
+            projectName={project.projectName}
+            taskDescription1={project.taskDescription1}
+            taskDescription2={project.taskDescription2}
+            taskDescription3={project.taskDescription3}
+            githubLink={project.githubLink}
+            deployedLink={project.deployedLink}
+            previewImage={project.previewImageLink}
+          />
+        ))
+      )}
+      <Footer />
     </div>
   );
 };
